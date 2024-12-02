beb20449b247b1446bde1f9f285e272bce4dfbb4
KaustubhSiriki <kaustubh.siriki@gmail.com>
2024-12-02 18:15:20 -0500

    basic dashboard complete w/o AWS
    basic dashboard design set-up
    - settings
    - products
    - expenses
    - inventory

--- server/dist/src/index.js
+++ server/dist/src/index.js
@@ -10,7 +10,9 @@ const cors_1 = __importDefault(require("cors"));
 const helmet_1 = __importDefault(require("helmet"));
 const morgan_1 = __importDefault(require("morgan"));
 const dashboardRoutes_1 = __importDefault(require("./routes/dashboardRoutes"));
-/* CONFIGURATIONS */
+const productRoutes_1 = __importDefault(require("./routes/productRoutes"));
+const userRoutes_1 = __importDefault(require("./routes/userRoutes"));
+const expenseRoutes_1 = __importDefault(require("./routes/expenseRoutes"));
 dotenv_1.default.config();
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
@@ -20,9 +22,10 @@ app.use((0, morgan_1.default)("common"));
 app.use(body_parser_1.default.json());
 app.use(body_parser_1.default.urlencoded({ extended: false }));
 app.use((0, cors_1.default)());
-/* ROUTES */
 app.use("/dashboard", dashboardRoutes_1.default);
-/* SERVER */
+app.use("/products", productRoutes_1.default);
+app.use("/users", userRoutes_1.default);
+app.use("/expenses", expenseRoutes_1.default);
 const port = Number(process.env.PORT) || 3001;
 app.listen(port, "0.0.0.0", () => {
     console.log(`Server running on port ${port}`);
--- server/src/index.ts
+++ server/src/index.ts
@@ -6,8 +6,10 @@ import helmet from "helmet";
 import morgan from "morgan";
 
 import dashboardRoutes from "./routes/dashboardRoutes";
+import productRoutes from "./routes/productRoutes";
+import userRoutes from "./routes/userRoutes";
+import expenseRoutes from "./routes/expenseRoutes";
 
-/* CONFIGURATIONS */
 dotenv.config();
 const app = express();
 app.use(express.json());
@@ -18,10 +20,11 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 
-/* ROUTES */
 app.use("/dashboard", dashboardRoutes);
+app.use("/products", productRoutes);
+app.use("/users", userRoutes);
+app.use("/expenses", expenseRoutes);
 
-/* SERVER */
 const port = Number(process.env.PORT) || 3001;
 app.listen(port, "0.0.0.0", () => {
   console.log(`Server running on port ${port}`);
